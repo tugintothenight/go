9f777f15cf890414313d7157ad5c640678acbc0e
nightmare^^ <nghtung1408@gmail.com>
2025-01-10 17:20:00 +0700

    oop

--- 1.go
+++ 1.go
@@ -1,16 +1,47 @@
 package main
+
 import "fmt"
 
-type Me struct {
-	name string
-	age int
+type Informer interface {
+	Infor()
 }
 
-func (m Me) Infor {
-	fmt.Printf("My name is %s, I'm %d years old", m.name, m.age)
+type Me struct {
+	Name string
+	Age  int
+}
+
+type Laptop struct {
+	Owner Me
+	Name  string
+	Price int
+}
+
+func newMe(name string, age int) Me {
+	return Me{Name: name, Age: age}
+}
+
+func newLaptop(nameOwner string, ageOwner int, nameLaptop string, price int) Laptop {
+	me := newMe(nameOwner, ageOwner)
+	laptop := Laptop{Owner: me, Name: nameLaptop, Price: price}
+	return laptop
+}
+
+func (p Me) Infor() {
+	fmt.Printf("name: %s age: %d\n", p.Name, p.Age)
+}
+
+func (l Laptop) Infor() {
+	fmt.Printf("Owner: %s, Age: %d, Laptop: %s, Price: %d\n", l.Owner.Name, l.Owner.Age, l.Name, l.Price)
+}
+
+func printInfo(i Informer) {
+	i.Infor()
 }
 
 func main() {
-	me := Me{"Tung", 20}
-	me.Infor()
+	me := newMe("Tung", 20)
+	laptop := newLaptop("Tung", 21, "Dell", 1000)
+	printInfo(me)
+	printInfo(laptop)
 }
